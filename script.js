54fb895f181a0b9224a646fdbfe5fb4efc8bb8bf
Syamodra <codesyamodra@gmail.com>
2025-09-29 18:03:37 +0700

    Modify the html and css file for new display
    Changing the display from input to div

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+  }
+}
+
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    currentInput += value;
+
+    display.textContent = currentInput;
+  });
+});
